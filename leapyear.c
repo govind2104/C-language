74e4f7507d69169e38b621d17318df42596c9c13
Govind Kumar <govind09bgs@gmail.com>
2025-11-13 20:32:50 +0530

    auto-commit

--- leapyear.c
+++ leapyear.c
@@ -6,10 +6,13 @@ int main() {
     printf("Enter a year: ");
     scanf("%d", &year);
 
-    // Leap year condition:
-    // 1. Divisible by 4 and not by 100, OR
-    // 2. Divisible by 400
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    /* Leap year condition:
+    1. Divisible by 4 and not by 100, OR
+     2. Divisible by 400*/
+
+     
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
         printf("%d is a leap year.\n", year);
     } else {
         printf("%d is not a leap year.\n", year);
